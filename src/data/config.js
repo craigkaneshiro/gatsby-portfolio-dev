11e06167d7701ab72f2154c6bf6334fad9d15aac
craig kaneshiro <craig.kaneshiro@gmail.com>
2020-06-11 16:44:37 -0700

    initial commit

---
+++ src/data/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  defaultTitle: 'Craig Kaneshiro',
+  logo: '/favicon/favicon-512.png',
+  author: 'Craig Kaneshiro',
+  url: 'https://www.google.com',
+  legalName: 'Craig Kaneshiro',
+  defaultDescription: 'I’m Craig and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/ckphantom1',
+    github: 'https://github.com/craigkaneshiro',
+    linkedin: 'https://www.linkedin.com/in/craig-kaneshiro-48a6bb11/',
+    instagram: 'https://instagram.com/cskaneshiro',
+    youtube: 'https://www.youtube.com/user/ckphantom1'
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@smakosh',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+};
